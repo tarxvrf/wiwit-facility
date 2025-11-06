5c83873495e913cea5c9021ef265887815a4954f
tarxvrf <tarxvrf@gmail.com>
2025-11-06 17:29:57 +0700

    perbaikan code

--- src/app/Hero.tsx
+++ src/app/Hero.tsx
@@ -6,12 +6,12 @@ import Image from "next/image";
 function Hero() {
 
   return (
-    <section className="pt-28 bg-[url('/images/gbherobg.jpg')] bg-cover bg-center bg-no-repeat flex items-center justify-center ">
+    <section className="pt-28 bg-[url('/images/gbherobg.jpg')] h-[800px] not-only-of-type:bg-cover bg-center bg-no-repeat flex items-center justify-center ">
       <div className="">
-        <div className="container mx-auto px-6 flex flex-col md:flex-row items-center justify-between gap-10">
+        <div className="container mx-auto px-6 flex flex-col md:grid md:grid-cols-2 items-center justify-between gap-10">
           {/* Kiri: Teks */}
-          <div data-aos="fade-up" data-aos-delay="0" className="flex-2 text-center md:text-left sm:-translate-y-30">
-            <h1 className="text-5xl md:text-7xl pt-10 font-bold text-gray-900 mb-4 leading-tight">
+          <div data-aos="fade-up" data-aos-delay="0" className="flex-1 text-center md:text-left ">
+            <h1 className="text-5xl md:text-8xl pt-10 font-bold text-gray-900 mb-4 leading-tight">
               <span className="text-orange-600">Solusi</span> Bisnis Anda
             </h1>
             <p className="text-4xl text-gray-600 mb-6">
@@ -30,14 +30,14 @@ function Hero() {
 
           {/* Kanan: Gambar */}
           <div data-aos="fade-up" data-aos-delay="500" className="flex-1 flex justify-center w-full relative ">
-            <div className="flex flex-col relative sm:translate-y-64 w-full max-w-md md:max-w-none md:w-full">
+            <div className="flex-col relative w-full max-w-md md:max-w-none md:w-full">
 
               <Image
                 width={1000}
                 height={1000}
-                src="/images/tigawanirembg.png"
+                src="/images/beranda.jpg"
                 alt="Smart Parking"
-                className="sm:block hidden w-[410px] object-cover relative sm:-translate-y-64"
+                className="sm:block hidden w-full object-cover  "
               />
 
 
